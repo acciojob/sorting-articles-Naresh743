de3476d7b057eaf611a1cc3ececd2fb5f1803a15
Naresh743 <nareshdevra743@gmail.com>
2025-02-07 10:32:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+//your JS code here. If required
+let arr = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+for (let i=0;i<arr.length;i++){
+	let newArr = arr[i].split("")
+	console.log(newArr);
+	if(newArr[0]=="A"|| newArr[0]=="An" || newArr[0]=="The"){
+		newArr.splice(0,1);
+	}
+	console.log(newArr)
+	obj[arr[i]] = newArr.join();
+	console.log(obj);
+}
